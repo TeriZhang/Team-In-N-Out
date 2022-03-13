375ef8a2991347c5a803d74c334e3ba3427f5192
TeriZhang <55502531+TeriZhang@users.noreply.github.com>
2022-03-12 20:13:57 -0800

    231231232121312

--- Assets/Scripts/MenuAnimController.cs
+++ Assets/Scripts/MenuAnimController.cs
@@ -6,6 +6,30 @@ using UnityEngine.SceneManagement;
 
 public class MenuAnimController : MonoBehaviour
 {
+    public List<GameObject> start;
+
+    public int current;
+
+    //Timer Related
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+        start[current].gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            current += 1;
+            start[current].gameObject.SetActive(true);
+            start[current - 1].gameObject.SetActive(false);
+        }
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
